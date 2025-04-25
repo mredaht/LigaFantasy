fd8c9cd7a6623cddfd90785f0f973e272eafef2a
mredaht <687099742@uma.es>
2025-04-25 19:46:54 +0200

    script minteo nft

--- script/MintPlayers.s.sol
+++ script/MintPlayers.s.sol
@@ -9,14 +9,22 @@ contract MintPlayers is Script {
     function run() external {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         address nftContractAddress = vm.envAddress("NFT_CONTRACT_ADDRESS");
+        address owner = vm.addr(privateKey);
 
         vm.startBroadcast(privateKey);
 
         FantasyPlayerNFT nft = FantasyPlayerNFT(nftContractAddress);
         SavePlayers.PlayerData[] memory players = SavePlayers.getPlayers();
 
-        for (uint256 i = 0; i < players.length; i++) {
-            nft.mintPlayer(msg.sender, players[i].name, players[i].team);
+        uint256 batchSize = 20;
+        uint256 start = vm.envOr("START_INDEX", uint256(0));
+        uint256 end = start + batchSize;
+        if (end > players.length) {
+            end = players.length;
+        }
+
+        for (uint256 i = start; i < end; i++) {
+            nft.mintPlayer(owner, players[i].name, players[i].team);
         }
 
         vm.stopBroadcast();
